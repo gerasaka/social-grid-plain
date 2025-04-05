57fad5f2d84d56f1bd43c15b2df9a87e8eed5bd7
gerasaka <gerryjulioo@gmail.com>
2025-04-05 13:15:30 +0700

    feat: style comment modal

--- components/comment-modal.ts
+++ components/comment-modal.ts
@@ -1,4 +1,5 @@
 import { getPostComments } from '../utils/comments';
+import { Comment } from '../utils/types';
 
 export class CommentModal extends HTMLElement {
   dialog: HTMLDialogElement;
@@ -8,38 +9,63 @@ export class CommentModal extends HTMLElement {
     this.dialog = document.createElement('dialog');
   }
 
-  private handleCloseModal = () => {
-    this.dialog.close();
-  };
-
   connectedCallback() {
+    this.dialog.setAttribute(
+      'class',
+      'backdrop:bg-[rgb(0,0,0,0.4)] bg-white p-5 my-auto mx-5 md:mx-auto rounded-xl max-w-[500px]'
+    );
     this.dialog.innerHTML = `
-      <h2>Comments</h2>
-      <ul id="comments-list"></ul>
-      <button id="close-modal">Close</button>
+      <div class="flex flex-col max-h-[calc(100svh-5rem)]">
+        <div class="flex justify-between items-center">
+          <h2 class="text-xl font-bold text-sky-800">Comments</h2>
+          <button id="close-modal" class="show-comments text-xs text-white bg-sky-800 px-4 py-2 rounded-md flex gap-2 items-center w-max hover:bg-sky-700 hover:cursor-pointer">Close</button>
+        </div>
+        <ul id="comments-list" class="my-4 max-h-full flex-1 overflow-y-auto"></ul>
+      </div>
     `;
     this.appendChild(this.dialog);
-    const closeButton = this.dialog.querySelector('#close-modal')!;
 
-    closeButton.addEventListener('click', this.handleCloseModal);
+    const closeButton = this.dialog.querySelector('#close-modal')!;
+    closeButton.addEventListener('click', this.close);
   }
 
   disconnectedCallback() {
     const closeButton = this.dialog.querySelector('#close-modal')!;
 
-    closeButton.removeEventListener('click', this.handleCloseModal);
+    closeButton.removeEventListener('click', this.close);
   }
 
-  async open(postId: number) {
+  createCommentItem(comment: Comment) {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'border-b border-sky-200 py-4');
+    li.innerHTML = `
+      <p class="font-semibold">${comment.name}</p>
+      <p class="text-xs text-gray-400">(${comment.email})</p>
+      <p class="mt-2 text-gray-700">${comment.body}</p>
+    `;
+    return li;
+  }
+
+  open = async (postId: number) => {
     const comments = await getPostComments(postId);
     const list = this.dialog.querySelector('#comments-list')!;
 
-    list.innerHTML = comments
-      .map((c) => `<li><strong>${c.email}:</strong> ${c.body}</li>`)
-      .join('');
+    list.innerHTML = '';
+
+    if (comments.length === 0) list.innerHTML = '<li>No comments available.</li>';
+    else {
+      comments.forEach((comment) => {
+        const li = this.createCommentItem(comment);
+        list.appendChild(li);
+      });
+    }
 
     this.dialog.showModal();
-  }
+  };
+
+  close = () => {
+    this.dialog.close();
+  };
 }
 
 customElements.define('comment-modal', CommentModal);
